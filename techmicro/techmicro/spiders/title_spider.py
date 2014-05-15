d93d5b2b8e99069c982c7b12e71dd89aeaa09254
Behzad <behzad.abghari@gmail.com>
2014-05-15 16:01:24 -0400

    multi-word search capability added

--- techmicro/techmicro/spiders/title_spider.py
+++ techmicro/techmicro/spiders/title_spider.py
@@ -10,55 +10,70 @@ class technetSpider(Spider):
     
     def __init__(self, keyword = None, *args, **kwargs):
         super(technetSpider, self).__init__(*args, **kwargs)
-        #self.search = techmicroItem(keyword = keyword)
-        #self.keyword = keyword
+        self.search = techmicroItem()
         self.ConfigSettings()
-        try:
-            self.keyword = keyword.lower()
-            self.file_keyword = "pages with %s" %self.keyword
-            open(self.file_keyword, 'w').close()
-        except AttributeError:
-            self.log('No keyword entered to search for in webpages!')
+        if self.keywords:
+            open("pages with keyword(s)", 'w').close()
+        else:
+            self.log('No keyword entered to search for!')
         self.totalCount = 0
         self.number = 0
 
-    #start_urls = ["https://technet.microsoft.com/en-us/library/security/dn631937.aspx"]
 
     def ConfigSettings(self):
         self.start_urls = []
         config = ConfigParser.ConfigParser()
         config.read("/home/behzad/scrapyproject/techmicro/spider_settings.ini")
-        self.start_urls.append(config.get('website', 'start_urls'))
-        self.keyword = config.get('search', 'keywords')
-        self.title_xpath = config.get('xpaths', 'title')
-        self.date_xpath = config.get('xpaths', 'date')
-        nodes = [x for x in config.options('xpaths') if x.startswith('node_')]
+        self.start_urls.append(config.get('website_1', 'start_urls'))
+        self.link_xpaths = config.get('website_1', 'link_01')     
+        keywords = [x for x in config.options('website_1') if x.startswith('keyword_')]
+        self.keywords = []
+        for keyword in keywords:
+            s = config.get('website_1', keyword)
+            self.keywords.append(s.lower())
+        self.title_xpath = config.get('website_1', 'title')
+        self.date_xpath = config.get('website_1', 'date')
+        nodes = [x for x in config.options('website_1') if x.startswith('node_')]
         self.node_xpath = []
         for node in nodes:
-            self.node_xpath.append(config.get('xpaths', node)) 
+            self.node_xpath.append(config.get('website_1', node)) 
        
 
     def parse(self, response):
+        keywords_dict = {}
+        flag = False
         self.sel = Selector(response)
-        count = self.getCount(response)
-        if count:
-            self.number += 1
-            f = open(self.file_keyword, 'a')
-            f.write("%d)%s\n" %(self.number, self.getTitle()) + response.url + "\n" + "count = %d, %s" %(count, self.getDate()) + "\n\n")
-            f.close() 
-        for url in self.sel.xpath('//div[re:test(@class, "toclevel2")]//a/@href').extract():
+        for keyword in self.keywords:
+            count = self.getCount(response, keyword)
+            keywords_dict[keyword] = count
+            if count:
+                self.number += 1
+                flag = True
+        if flag:
+            self.write_to_file(response, keywords_dict)
+        for url in self.sel.xpath(self.link_xpaths).extract():
             yield Request(url, callback = self.parse) 
+
+
+    def write_to_file(self, response, keywords_dict):
+        with open("pages with keyword(s)", 'a') as f:
+            f.write("%d)%s\n" %(self.number, self.getTitle()) + response.url + "\n")
+            for p in keywords_dict.items():                
+                f.write("%s : %s \n" %p)
+            f.write("\n")
     
-    def getCount(self, response):
+
+    def getCount(self, response, keyword):
         count = 0
         if not response.url.count("technet.microsoft.com/en-us/library/security/ms"):
             return count
         for node in self.node_xpath:
             for p in self.sel.xpath(node).extract():
                 lowerp = p.lower()
-                count += lowerp.count(self.keyword)
+                count += lowerp.count(keyword)
         self.totalCount += count 
         return count   
+
 
     def getDate(self): 
         self.search['date'] = self.sel.xpath(self.date_xpath).extract()
@@ -66,6 +81,7 @@ class technetSpider(Spider):
         if date:
             return date[0]
         return ''
+
 
     def getTitle(self):
         self.search['title'] = self.sel.xpath(self.title_xpath).extract()
