eefe475ea1b85398a5f04b334f57700369ff4b3e
Behzad <behzad.abghari@gmail.com>
2014-05-20 16:08:55 -0400

    Searching through Securityfocus.com enabled

--- techmicro/techmicro/spiders/title_spider.py
+++ techmicro/techmicro/spiders/title_spider.py
@@ -4,18 +4,21 @@ from scrapy.spider import Spider
 from scrapy import log
 from techmicro.items import techmicroItem
 import ConfigParser
+import sys
 
 class technetSpider(Spider):
     name = "technet"
     
-    def __init__(self, keyword = None, *args, **kwargs):
+    def __init__(self, website = "website_1", *args, **kwargs):
         super(technetSpider, self).__init__(*args, **kwargs)
+        self.website = website
         self.search = techmicroItem()
         self.ConfigSettings()
         if self.keywords:
-            open("pages with keyword(s)", 'w').close()
+            open("pages with keyword(s) - " + self.name, 'w').close()
         else:
             self.log('No keyword entered to search for!')
+            sys.exit(0)
         self.totalCount = 0
         self.number = 0
 
@@ -24,19 +27,32 @@ class technetSpider(Spider):
         self.start_urls = []
         config = ConfigParser.ConfigParser()
         config.read("/home/behzad/scrapyproject/techmicro/spider_settings.ini")
-        self.start_urls.append(config.get('website_1', 'start_urls'))
-        self.link_xpaths = config.get('website_1', 'link_01')     
-        keywords = [x for x in config.options('website_1') if x.startswith('keyword_')]
+        if self.website not in config.sections():
+            self.log('Website does not exist in spider_settings.ini')
+            self.log('Program shutdown ...')
+            sys.exit(0)
+        self.name = config.get(self.website, 'name')
+        self.start_urls.append(config.get(self.website, 'start_urls'))
+        links = [x for x in config.options(self.website) if x.startswith('link_')]
+        self.link_xpaths = []
+        for link in links:
+            prefix = "prefix_" + link
+            if prefix in config.options(self.website):
+                prefix = config.get(self.website, prefix)
+            else:
+                prefix = ""
+            self.link_xpaths.append([prefix, config.get(self.website, link)]) 
+        keywords = [x for x in config.options(self.website) if x.startswith('keyword_')]
         self.keywords = []
         for keyword in keywords:
-            s = config.get('website_1', keyword)
+            s = config.get(self.website, keyword)
             self.keywords.append(s.lower())
-        self.title_xpath = config.get('website_1', 'title')
-        self.date_xpath = config.get('website_1', 'date')
-        nodes = [x for x in config.options('website_1') if x.startswith('node_')]
+        self.title_xpath = config.get(self.website, 'title')
+        self.date_xpath = config.get(self.website, 'date')
+        nodes = [x for x in config.options(self.website) if x.startswith('node_')]
         self.node_xpath = []
         for node in nodes:
-            self.node_xpath.append(config.get('website_1', node)) 
+            self.node_xpath.append(config.get(self.website, node)) 
        
 
     def parse(self, response):
@@ -51,12 +67,13 @@ class technetSpider(Spider):
                 flag = True
         if flag:
             self.write_to_file(response, keywords_dict)
-        for url in self.sel.xpath(self.link_xpaths).extract():
-            yield Request(url, callback = self.parse) 
+        for link in self.link_xpaths:
+            for url in self.sel.xpath(link[1]).extract():
+                yield Request(link[0]+url, callback = self.parse) 
 
 
     def write_to_file(self, response, keywords_dict):
-        with open("pages with keyword(s)", 'a') as f:
+        with open("pages with keyword(s) - " + self.name, 'a') as f:
             f.write("%d)%s\n" %(self.number, self.getTitle()) + response.url + "\n")
             for p in keywords_dict.items():                
                 f.write("%s : %s \n" %p)
@@ -65,8 +82,8 @@ class technetSpider(Spider):
 
     def getCount(self, response, keyword):
         count = 0
-        if not response.url.count("technet.microsoft.com/en-us/library/security/ms"):
-            return count
+        #if not response.url.count("technet.microsoft.com/en-us/library/security/ms"):
+        #    return count
         for node in self.node_xpath:
             for p in self.sel.xpath(node).extract():
                 lowerp = p.lower()
